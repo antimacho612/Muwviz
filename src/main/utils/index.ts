31e31770f42892aa7bd8aff4abc50b2965b455bd
ttatto <zwcxk5131@gmail.com>
2024-01-10 19:00:49 +0900

    ♻️refactoring

--- src/main/utils/index.ts
+++ src/main/utils/index.ts
@@ -4,27 +4,24 @@ import fs from 'fs';
 import path from 'path';
 import { showWindow } from '@main/window';
 
-export const ensureDirectory = async (path: string) =>
+export const ensureDirectory = async (path: string) => {
   await fsAsync.mkdir(path, { recursive: true });
+};
 
 export const deleteOldLog = (dir: string, days = 3) => {
   const daysAgo = new Date();
   daysAgo.setDate(daysAgo.getDate() - days);
 
   fs.readdir(dir, (err, files) => {
-    if (err || !files.length) {
-      return;
-    }
+    if (err || !files.length) return;
 
     for (const file of files) {
-      const matches = /^(\d{4}-\d{2}-\d{2})\.log$/.exec(file);
+      const matches = /^(\d{4}-\d{2}-\d{2})(\.old)?\.log$/.exec(file);
       if (matches) {
         const fileDate = new Date(matches[1]);
         if (fileDate.getTime() < daysAgo.getTime()) {
           fs.unlink(path.join(dir, file), (err) => {
-            if (err) {
-              console.error(err);
-            }
+            if (err) console.error(err);
           });
         }
       }
